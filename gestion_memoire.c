7a7eaf580279beef6520c2abbfa6ff0b95fe6495
Samuel JIMENEZ <sjimenez@e1r8p19.42.fr>
2018-03-09 23:28:06 +0100

    First lines :)

--- gestion_memoire.c
+++ gestion_memoire.c
@@ -3,11 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   gestion_memoire.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sjimenez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sjimenez <sjimenez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/09 22:47:20 by sjimenez          #+#    #+#             */
-/*   Updated: 2018/03/09 22:47:22 by sjimenez         ###   ########.fr       */
+/*   Updated: 2018/03/09 23:27:37 by sjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+static char			*get_arena(void)
+{
+	static char		arena[MEM_SIZE];
+
+	return (arena);
+}
+
+# define MODE_DIRECT 0xF0
+# define MODE_INDIRECT 0x0F
+
+void				write_reg(int adresse, t_process process, char mode)
+{
+	// Faire % IDX_MOD
+	if (mode & MODE_DIRECT)
+	{
+		//Ecriture Direct
+	}
+	else
+	{
+		//Ecriture Indirect
+	}
+}
+
+void				read_memory()
+{
+
+}
+
+void				print_memory()
+{
+
+}

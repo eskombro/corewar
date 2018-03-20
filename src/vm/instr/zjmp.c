98f73527d409578261ae266629e21380a45b5df3
Hugo BOUILLOT <hbouillo@e1r5p10.42.fr>
2018-03-21 00:21:20 +0100

    lol lol LOL lol

--- src/vm/instr/zjmp.c
+++ src/vm/instr/zjmp.c
@@ -6,7 +6,7 @@
 /*   By: bacrozat <bacrozat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 22:45:40 by bacrozat          #+#    #+#             */
-/*   Updated: 2018/03/19 21:44:55 by hbouillo         ###   ########.fr       */
+/*   Updated: 2018/03/21 00:21:05 by hbouillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ void		zjmp(void *or_proc)
 	proc = (t_proc*)or_proc;
 	instr = proc->current_task;
 	if (proc->carry)
-		proc->pc += get_parameter_result(instr->par[0], proc, 0);
+		proc->pc += get_parameter_result(instr->par[0], proc, 0) % IDX_MOD;
 }

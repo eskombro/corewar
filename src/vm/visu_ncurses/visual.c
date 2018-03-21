cd5f50b21d27d91c32bfc79c4d7c71d87f113b7b
Samuel JIMENEZ <sjimenez@e1r2p13.42.fr>
2018-03-22 00:16:58 +0100

    visu 0.2

--- src/vm/visu_ncurses/visual.c
+++ src/vm/visu_ncurses/visual.c
@@ -6,7 +6,7 @@
 /*   By: sjimenez <sjimenez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/17 19:43:36 by sjimenez          #+#    #+#             */
-/*   Updated: 2018/03/21 04:13:38 by sjimenez         ###   ########.fr       */
+/*   Updated: 2018/03/22 00:16:35 by sjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ void		initialize_arena_visu(t_logic *logic, uchar *arena_writer)
 	int				i;
 
 	champ = -1;
-	initscr();
 	start_color();
 	init_pair(1, COLOR_BLUE, COLOR_BLACK);
 	init_pair(2, COLOR_GREEN, COLOR_BLACK);
@@ -33,12 +32,12 @@ void		initialize_arena_visu(t_logic *logic, uchar *arena_writer)
 	init_pair(4, COLOR_YELLOW, COLOR_BLACK);
 	init_pair(5, COLOR_WHITE, COLOR_BLACK);
 	init_pair(6, COLOR_CYAN, COLOR_BLACK);
-	init_pair(11, COLOR_BLUE, COLOR_WHITE);
-	init_pair(12, COLOR_GREEN, COLOR_WHITE);
-	init_pair(13, COLOR_RED, COLOR_WHITE);
-	init_pair(14, COLOR_YELLOW, COLOR_WHITE);
-	init_pair(15, COLOR_WHITE, COLOR_WHITE);
-	init_pair(16, COLOR_CYAN, COLOR_WHITE);
+	init_pair(11, COLOR_BLACK, COLOR_WHITE);
+	init_pair(12, COLOR_BLACK, COLOR_WHITE);
+	init_pair(13, COLOR_BLACK, COLOR_WHITE);
+	init_pair(14, COLOR_BLACK, COLOR_WHITE);
+	init_pair(15, COLOR_BLACK, COLOR_WHITE);
+	init_pair(16, COLOR_BLACK, COLOR_WHITE);
 	while (++champ < logic->players_count)
 	{
 		i = logic->champs[champ].spawn - 1;
@@ -91,82 +90,96 @@ void		visu_handle_sti(t_proc *process)
 
 void		update_arena_visu(t_proc *process)
 {
-
 	if (process->current_task->opcode == 3)
 		visu_handle_st(process);
 	else if (process->current_task->opcode == 11)
 		visu_handle_sti(process);
 }
 
-void		display_scoreboard(t_logic *logic)
+void		display_line_separator(void)
 {
 	int				x;
-	int				last_line;
 
-	attron(COLOR_PAIR(6));
 	x = -1;
-	while (++x < (COLS - ((64 * 2) + 63) )/2)
+	attron(COLOR_PAIR(6));
+	while (++x < (COLS - ((64 * 2) + 63)) / 2)
 		printw(" ");
 	printw("------------------------------------------------------------");
 	printw("------------------------------------------------------------");
 	printw("------------------------------------------------------------");
 	printw("-----------\n");
-	x = -1;
-	while (++x < (COLS - ((64 * 2) + 63) )/2)
-		printw(" ");
-	printw("   Cycles:           %d\n", logic->cycles);
-	x = -1;
-	while (++x < (COLS - ((64 * 2) + 63) )/2)
-		printw(" ");
-	printw("   Cycles to die:    %d\n", logic->cycles_to_die);
-	x = -1;
-	while (++x < (COLS - ((64 * 2) + 63) )/2)
-		printw(" ");
-	printw("   Cycles left:      %d\n", logic->cycles_left);
+}
+
+void		display_players_info(t_logic *logic)
+{
+	int				x;
+	int				last_line;
+
 	x = -1;
 	last_line = (MEM_SIZE / 64) + 2;
 	while (++x < logic->players_count)
 	{
 		attron(COLOR_PAIR(logic->champs[x].fixed_id + 1));
-		move(last_line++, (COLS - ((64 * 2) + 63) )/2 + 60);
-		printw("   Player:           ", logic->champs[x].name, logic->champs[x].id);
-		printw("%s (%d) \n", logic->champs[x].name, logic->champs[x].id);
-		move(last_line++, (COLS - ((64 * 2) + 63) )/2 + 60);
+		move(last_line++, (COLS - ((64 * 2) + 63)) / 2 + 60);
+		printw("   ");
+		printw("Player:        ", logic->champs[x].name, logic->champs[x].id);
+		printw("   %s (%d) \n", logic->champs[x].name, logic->champs[x].id);
+		move(last_line++, (COLS - ((64 * 2) + 63)) / 2 + 60);
 		printw("   Lives:            %d \n", logic->champs[x].lives);
 	}
 }
 
-void		display_arena(uchar *arena)
+void		display_scoreboard(t_logic *logic)
 {
 	int				x;
+
+	x = -1;
+	display_line_separator();
+	while (++x < (COLS - ((64 * 2) + 63)) / 2)
+		printw(" ");
+	printw("   Cycles:           %d\n", logic->cycles);
+	x = -1;
+	while (++x < (COLS - ((64 * 2) + 63)) / 2)
+		printw(" ");
+	printw("   Cycles to die:    %d\n", logic->cycles_to_die);
+	x = -1;
+	while (++x < (COLS - ((64 * 2) + 63)) / 2)
+		printw(" ");
+	printw("   Cycles left:      %d\n", logic->cycles_left);
+	display_players_info(logic);
+}
+
+void		display_arena_line(uchar *arena, uchar *arena_visu, int y)
+{
+	int				x;
+
+	x = -1;
+	while (++x < (COLS - ((64 * 2) + 63)) / 2)
+		printw(" ");
+	x = -1;
+	while (++x < 64)
+	{
+		attron(COLOR_PAIR((int)(arena_visu[(y * 64) + x] + 1)));
+		arena[(y * 64) + x] <= 0xF ? printw("0") : 0;
+		printw("%x", arena[(y * 64) + x]);
+		attron(COLOR_PAIR(1));
+		printw(" ");
+	}
+	printw("\n");
+}
+
+void		display_arena(uchar *arena)
+{
 	int				y;
 	uchar			*arena_visu;
 
-	x = -1;
 	y = -1;
 	move(0, 0);
 	attron(COLOR_PAIR(6));
 	arena_visu = get_arena_visu();
-	while (++x < (COLS - ((64 * 2) + 63) )/2)
-		printw(" ");
-	printw("------------------------------------------------------------");
-	printw("------------------------------------------------------------");
-	printw("------------------------------------------------------------");
-	printw("-----------\n");
+	display_line_separator();
 	while (++y < (MEM_SIZE / 64))
-	{
-		x = -1;
-		while (++x < (COLS - ((64 * 2) + 63) )/2)
-			printw(" ");
-		x = -1;
-		while (++x < 64)
-		{
-			attron(COLOR_PAIR((int)(arena_visu[(y * 64) + x] + 1)));
-			arena[(y * 64) + x] <= 0xF ? printw("0") : 0;
-			printw("%x ", arena[(y * 64) + x]);
-		}
-		printw("\n");
-	}
+		display_arena_line(arena, arena_visu, y);
 }
 
 void		display_process(t_logic *logic)
@@ -174,15 +187,18 @@ void		display_process(t_logic *logic)
 	t_llist				*tmp;
 	uchar				*arena_visu;
 	t_proc				*proc;
-	int		p;
+	int					p;
 
 	p = -1;
 	arena_visu = get_arena_visu();
+	while (++p < MEM_SIZE)
+		arena_visu[p] >= 10 ? arena_visu[p] -= 10 : 0;
 	tmp = logic->queue;
 	while (tmp)
 	{
 		proc = (t_proc *)tmp->data;
-		arena_visu[(proc->owner->spawn + proc->pc) % MEM_SIZE] += 9;
+		p = (proc->owner->spawn + proc->pc) % MEM_SIZE;
+		arena_visu[p] <= 10 ? arena_visu[p] += 10 : 0;
 		tmp = tmp->next;
 	}
 }
@@ -194,12 +210,13 @@ void		print_screen(t_logic *logic)
 	uchar			*arena_visu;
 	int				delay;
 
-	delay = 100;
+	initscr();
+	delay = 1000;
 	arena = get_arena();
 	arena_visu = get_arena_visu();
 	!init++ ? initialize_arena_visu(logic, arena_visu) : 0;
 	display_arena(arena);
-	//display_process(logic);
+	display_process(logic);
 	display_scoreboard(logic);
 	refresh();
 	usleep(delay);

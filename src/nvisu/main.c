770d3bbcae9c819759a6b5f1da8041e3105d11b4
Eskombro <sjimenezre@gmail.com>
2018-04-08 06:45:02 +0200

    nice

--- src/nvisu/main.c
+++ src/nvisu/main.c
@@ -6,7 +6,7 @@
 /*   By: hbouillo <hbouillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/27 00:16:17 by hbouillo          #+#    #+#             */
-/*   Updated: 2018/04/08 05:29:26 by sjimenez         ###   ########.fr       */
+/*   Updated: 2018/04/08 06:42:28 by sjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,9 @@ void			display_arena(t_visu *v)
 	int				x;
 	int				y;
 
-	initscr();
-	start_color();
 
-	init_pair(1, 235, COLOR_BLACK);
-	init_pair(20, 242, COLOR_BLACK);
-	init_pair(10, COLOR_RED, COLOR_BLACK);
-	init_pair(11, 34, COLOR_BLACK);
-	init_pair(12, 214, COLOR_BLACK);
-	init_pair(13, 91, COLOR_BLACK);
 	y = -1;
-	v->start_y = 10;
+	v->start_y = 12;
 	v->start_x = (COLS - (MEM_TAB_LENGTH * 3)) / 2;
 	while (++y < (v->m_size / MEM_TAB_LENGTH))
 	{
@@ -52,12 +44,40 @@ void			display_arena(t_visu *v)
 	v->mem_tab_height = y;
 }
 
+void 			print_champ(t_uchar *champ, int champ_size, t_command *command)
+{
+	int			x;
+	int			y;
+	int			spawn_x;
+	int			spawn_y;
+	int			name_size;
+
+	y = -1;
+	x = 0;
+	name_size = read_short(command->data + 4);
+	spawn_y = get_visu()->start_y + ((read_int(command->data + 6 + name_size)) / MEM_TAB_LENGTH);
+	spawn_x = get_visu()->start_x + ((read_int(command->data + 6 + name_size)) % MEM_TAB_LENGTH);
+	attron(COLOR_PAIR(get_visu()->champ_nb + 10));
+	while (x + (++y * MEM_TAB_LENGTH) < champ_size)
+	{
+		x = -1;
+		while (++x < MEM_TAB_LENGTH)
+		{
+			if ((x + (y * MEM_TAB_LENGTH)) < champ_size)
+			{
+				move(spawn_y + y, spawn_x + (x * 3));
+				champ[x + (y * MEM_TAB_LENGTH)] < 16 ? printw("0") : 0;
+				printw("%x ", champ[x + (y * MEM_TAB_LENGTH)]);
+			}
+		}
+		x = 0;
+	}
+}
+
 void			handle_champ_spawn(t_command *command)
 {
 	int			name_size;
 	t_v_champ	*player;
-	int			spawn_x;
-	int			spawn_y;
 	t_uchar		*champ;
 	int			champ_size;
 
@@ -72,83 +92,15 @@ void			handle_champ_spawn(t_command *command)
 	if (!(player->name = ft_strnew(name_size + 1)))
 		exit(1);
 	ft_memcpy(player->name, command->data + 6, name_size);
-
-	spawn_y = get_visu()->start_y + ((read_int(command->data + 6 + name_size)) / 64);
-	spawn_x = get_visu()->start_x + ((read_int(command->data + 6 + name_size)) % 64);
 	champ_size = read_int(command->data + 6 + name_size + 4);
-	attron(COLOR_PAIR(get_visu()->champ_nb + 10));
-	move(spawn_y, spawn_x);
 	if (!(champ = (t_uchar*)ft_strnew(champ_size)))
 		exit(1);
 	ft_memcpy(champ, command->data + 6 + name_size + 8, read_int(command->data + 6 + name_size + 4));
-
-	int				x;
-	int				y;
-
-	y = -1;
-	x = 0;
-	while (x + (++y * 64) < champ_size)
-	{
-		x = -1;
-		while (++x < MEM_TAB_LENGTH)
-		{
-			if ((x + (y * 64)) < champ_size)
-			{
-				attron(COLOR_PAIR(get_visu()->champ_nb + 10));
-				move(spawn_y + y, spawn_x + (x * 3));
-				champ[x + (y * 64)] < 16 ? printw("0") : 0;
-				printw("%x ", champ[x + (y * 64)]);
-			}
-			else
-			{
-				attron(COLOR_PAIR(1));
-				printw("00 ");
-			}
-		}
-		x = 0;
-	}
+	print_champ(champ, champ_size, command);
 	attron(COLOR_PAIR(2));
 	free(champ);
 	get_visu()->champ_nb++;
-
-int		i;
-
-attron(COLOR_PAIR(20));
-
-i = 0;
-move(i++, (COLS - 83) / 2);
-printw("                                                                                     ");
-move(i++, (COLS - 83) / 2);
-printw("  .d8888b.   .d88888b.  8888888b.  8888888888 888       888        d8888 8888888b.   ");
-move(i++, (COLS - 83) / 2);
-printw(" d88P  Y88b d88P   Y88b 888   Y88b 888        888   o   888       d88888 888   Y88b  ");
-move(i++, (COLS - 83) / 2);
-printw(" 888    888 888     888 888    888 888        888  d8b  888      d88P888 888    888  ");
-move(i++, (COLS - 83) / 2);
-printw(" 888        888     888 888   d88P 8888888    888 d888b 888     d88P 888 888   d88P  ");
-move(i++, (COLS - 83) / 2);
-printw(" 888        888     888 8888888P   888        888d88888b888    d88P  888 8888888P    ");
-move(i++, (COLS - 83) / 2);
-printw(" 888    888 888     888 888 T88b   888        88888P Y88888   d88P   888 888 T88b    ");
-move(i++, (COLS - 83) / 2);
-printw(" Y88b  d88P Y88b. .d88P 888  T88b  888        8888P   Y8888  d8888888888 888  T88b   ");
-move(i++, (COLS - 83) / 2);
-printw("   Y8888P     Y88888P   888   T88b 8888888888 888P     Y888 d88P     888 888   T88b  ");
-move(i++, (COLS - 83) / 2);
-printw("                                                                                     ");
-
-attron(COLOR_PAIR(1));
-
-/*int		pl;
-
-pl = -1;
-while (++pl < 256)
-{
-	init_pair(pl, pl, COLOR_BLACK);
-	attron(COLOR_PAIR(pl));
-	printw("%0.3d ", pl);
-}*/
-
+	attron(COLOR_PAIR(1));
 }
 
 void			handle_command(t_command *command)
@@ -162,9 +114,7 @@ void			handle_command(t_command *command)
 		display_arena(get_visu());
 	}
 	else if (command->type == COMMAND_CHAMP_SPAWN)
-	{
 		handle_champ_spawn(command);
-	}
 }
 
 void			read_loop(void)
@@ -172,6 +122,7 @@ void			read_loop(void)
 	t_command	command;
 	t_uchar		c[2];
 	int			ret;
+	int			i;
 
 	while ((ret = read(0, c, 1)) > 0)
 	{
@@ -183,16 +134,73 @@ void			read_loop(void)
 		ret = read(0, command.data, command.size);
 		if (ret != command.size)
 			break ;
-		debug_command(command);
+		//debug_command(command);
 		handle_command(&command);
 		refresh();
 	}
 	endwin();
-	//TO FREE:
-	// mem
-	// play_id
-	// play_name (**)
-	// each play_name
+	ft_strdel((char**)&get_visu()->mem);
+	i = 0;
+	while (get_visu()->champs[i])
+	{
+		free(get_visu()->champs[i]->name);
+		free(get_visu()->champs[i++]);
+	}
+	free(get_visu()->champs);
+}
+
+void			initialize_ncurses(void)
+{
+	initscr();
+	start_color();
+
+	init_pair(1, 235, COLOR_BLACK);
+	init_pair(20, 242, COLOR_BLACK);
+	init_pair(10, COLOR_RED, COLOR_BLACK);
+	init_pair(11, 34, COLOR_BLACK);
+	init_pair(12, 214, COLOR_BLACK);
+	init_pair(13, 91, COLOR_BLACK);
+}
+
+void			print_header2(int y, int x)
+{
+	move(y++, x);
+	printw(" 888        888     888 888   d88P 8888888");
+	printw("    888 d888b 888     d88P 888 888   d88P ");
+	move(y++, x);
+	printw(" 888        888     888 8888888P   888    ");
+	printw("    888d88888b888    d88P  888 8888888P   ");
+	move(y++, x);
+	printw(" 888    888 888     888 888 T88b   888    ");
+	printw("    88888P Y88888   d88P   888 888 T88b   ");
+	move(y++, x);
+	printw(" Y88b  d88P Y88b. .d88P 888  T88b  888    ");
+	printw("    8888P   Y8888  d8888888888 888  T88b  ");
+	move(y++, x);
+	printw("   Y8888P     Y88888P   888   T88b 8888888");
+	printw("888 888P     Y888 d88P     888 888   T88b ");
+}
+
+void			print_header(void)
+{
+	int		y;
+	int		x;
+
+	attron(COLOR_PAIR(20));
+
+	y = 0;
+	x = (COLS - 83) / 2;
+	move(y++, x);
+	move(y++, x);
+	printw("  .d8888b.   .d88888b.  8888888b.  8888888");
+	printw("888 888       888        d8888 8888888b.  ");
+	move(y++, x);
+	printw(" d88P  Y88b d88P   Y88b 888   Y88b 888    ");
+	printw("    888   o   888       d88888 888   Y88b ");
+	move(y++, x);
+	printw(" 888    888 888     888 888    888 888    ");
+	printw("    888  d8b  888      d88P888 888    888 ");
+	print_header2(y, x);
 }
 
 int				main(int argc, char **argv)
@@ -200,6 +208,8 @@ int				main(int argc, char **argv)
 	if (argc < 2)
 		return (0);
 	run_corewar(argv);
+	initialize_ncurses();
+	print_header();
 	read_loop();
 	return (0);
 }
